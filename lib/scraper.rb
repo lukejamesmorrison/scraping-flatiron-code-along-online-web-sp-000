82c3dae21d78c775ab104384604f55e6279b6465
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-02 11:25:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,8 @@ require_relative './course.rb'
 
 class Scraper
 
-  def get_page(url)
-    doc = Nokogiri::HTML(open(url))
+  def get_page
+    doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
   end
 
   def print_courses
